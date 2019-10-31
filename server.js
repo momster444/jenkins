5e56eeb4bd18b30f1db089224a13222e76ace43c
momster444 <momster444@naver.com>
2019-10-31 15:34:36 +0900

    change server.js2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World'));
+app.get('/', (req,res) => res.send('Hello Nice to meet ypu'));
 
 app.listen(3000, () => console.log('123'));
