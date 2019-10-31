62c12c67a9afb1d7d98e1990c3839a04252b14c9
momster444 <momster444@naver.com>
2019-10-31 14:40:51 +0900

    first commit3

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
 const express = require('express');
-console.log('hello');
-console.log('jenkins');
-console.log('push');
-12321321
-console.log('push2');
-제발
-help me
-이제좀 되자
-console.log('hello');
