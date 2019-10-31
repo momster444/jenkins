2d57c7c00ae6171daa77575d86192d6d620d192b
momster444 <momster444@naver.com>
2019-10-31 16:14:18 +0900

    change server.js4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello Nice to meet you123456!!!'));
+app.get('/', (req,res) => res.send('Hello Nice to meet you123456asdf!!!'));
 
 app.listen(3000, () => console.log('123'));
